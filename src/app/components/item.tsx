86b32c73b6a3164c86b39323680833b359407371
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2024-04-05 07:42:36 +0000

    商品画像が表示されない不具合を修正

--- src/app/components/item.tsx
+++ src/app/components/item.tsx
@@ -22,7 +22,7 @@ const Item = ({ id, title, image, price, description, category }: props) => {
                 <p className="title">
                     {id}. {title}
                 </p>
-                <img src="{image}" alt="{title}" className="image" />
+                <img src={image} alt={title} className="image" />
                 <p className="price">${price}</p>
                 <p className="description">{description}</p>
                 <p className="category">{category}</p>
